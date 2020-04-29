be968ed3c8f4a637d8ed930681f51fdfb72f8cac
Nuno Polonia <nuno.polonia@financetechnology.com>
2020-04-29 21:22:29 +0100

    Remove assets folder from project

--- config/dev.exs
+++ config/dev.exs
@@ -12,15 +12,7 @@ config :api, ApiWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
